f6406448f024a51e6c3a92685ccf9b8bb3bc5f03
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-10-03 20:30:31 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,56 @@
-mod publisher;
-mod subscriber;
+use std::sync::{Arc,Mutex};
+use std_msgs::msg::String as StringMsg;
 
-fn main() {
-    std::thread::spawn(|| {
-        publisher::publish();
-    });
-
-    subscriber::subscribe();
+struct RepublisherNode {
+    node: rclrs::Node,
+    _subscription: Arc<rclrs::Subscription<StringMsg>>,
+    publisher: rclrs::Publisher<StringMsg>,
+    data: Arc<Mutex<Option<StringMsg>>>,
 }
+
+impl RepublisherNode {
+    fn new(context: &rclrs::Context) -> Result<Self, rclrs::RclrsError> {
+        let mut node = rclrs::Node::new(context, "republisher")?;
+        let data = Arc::new(Mutex::new(None));
+        let data_cb = Arc::clone(&data);
+        let _subscription = node.create_subscription(
+            "in_topic",
+            rclrs::QOS_PROFILE_DEFAULT,
+            move |msg: StringMsg| { 
+                *data_cb.lock().unwrap() = Some(msg);
+            },
+        )?;
+
+        let publisher = node.create_publisher("out_topic",rclrs::QOS_PROFILE_DEFAULT)?;
+
+        Ok(Self {
+            node,
+            _subscription,
+            publisher,
+            data,
+        })
+    }
+
+    fn republish(&self) -> Result<(),rclrs::RclrsError>{
+        if let Some(s) = &*self.data.lock().unwrap(){
+            self.publisher.publish(s)?;
+        }
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), rclrs::RclrsError> {
+    let context = rclrs::Context::new(std::env::args())?;
+    let republisher = Arc::new(RepublisherNode::new(&context)?);
+    let republisher_other_thread = Arc::clone(&republisher);　
+    std::thread::spawn(move || ->Result<(),rclrs::RclrsError> {
+        loop {
+            use std::time::Duration;
+            std::thread::sleep(Duration::from_millis(1000));
+            republisher_other_thread.republish()?;
+        }
+    });
+    rclrs::spin(&republisher.node)
+}
+
 
